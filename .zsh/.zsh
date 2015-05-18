5aecb4fe93218dd47b9f76a5dbc85029f7285278
eiji mishiro <eiji346g@gmail.com>
2015-05-18 17:33:28 +0700

    remove .zsh/

--- .zsh/.zsh
+++
@@ -1,1 +0,0 @@
-/Users/eiji/dotfiles/.zsh
--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,6 +1,5 @@
 #! /bin/bash 
 ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.zsh ~/.zsh
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 
